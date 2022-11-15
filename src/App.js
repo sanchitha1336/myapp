0091b8cb24f7bfb4c63a41750ceef690922ac31a
sanchitha1336 <sanchithashimoga@gmail.com>
2022-11-15 14:56:40 +0530

    product ecommerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUp from './SignUp';
+import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom';
+import Login from './Login';
+import Menu from './Menu';
+import CardList from './Card';
+import Home from './Home';
+import { createContext, useEffect, useState } from 'react';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Router>
+        <Routes>
+      {/* <Route exact path='/' element={<SignUp />} /> */}
+      {/* <Route exact path='/login' element={<Login/>} />
+      <Route exact path='/menu' element={<Menu/>} /> */}
+      <Route exact path='/home' element={<Home />} />
+      </Routes>
+      </Router>
     </div>
   );
 }
